0f5bbbb2166d5c06f1541ffd9d67dcb04e79ba4a
Juniper Song <songminseonk@gmail.com>
2023-01-10 13:04:08 -0500

    js intro

---
+++ htmlcssjs/index.js
@@ -0,0 +1,16 @@
+let foo = 'bar'
+console.log(foo)
+const bar = 'foo'
+
+var hello = 'world'
+var num = 123
+var boolean = true
+var arr = [1, 2, 3, 4]
+
+foo = 3
+console.log(foo)
+
+foo = false
+console.log(foo)
+
+bar = false
