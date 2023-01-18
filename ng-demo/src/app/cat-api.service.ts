2aee1196bcc586fb38745ec28149c8c08b27c02c
Juniper Song <songminseonk@gmail.com>
2023-01-18 15:27:44 -0500

    refactoring the template form to use property binding

--- ng-demo/src/app/cat-api.service.ts
+++ ng-demo/src/app/cat-api.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { voteDTO } from 'src/models/voteDTO';
 import { Observable } from 'rxjs';
-import { apikey } from 'src/api-key';
+import { apikey, apiroot } from 'src/api-key';
 
+interface CatObj {
+  id : string;
+  url : string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class CatApiService {
 
   constructor(private http: HttpClient) { }
-  private apiroot : string = "https://api.thecatapi.com/v1"
-  vote(args : voteDTO) : Observable<any> {
+  vote(args : voteDTO) : Observable<CatObj> {
     // object destructuring
     const {id, amount} = args;
     //the above line does the same thing as the below line
@@ -19,17 +22,17 @@ export class CatApiService {
     const payload = {"image_id": id, "value": amount}
 
     return this.http.post(
-      `${this.apiroot}/votes`, 
+      `${apiroot}/votes`, 
       payload, 
       {
           headers: {
               'x-api-key': apikey
           }
-        });
+        }) as Observable<CatObj>;
   }
 
   getCats(numCats : number) : Observable<any> {
-    const url = `${this.apiroot}/images/search?limit=${numCats}&has_breeds=1`
+    const url = `${apiroot}/images/search?limit=${numCats}&has_breeds=1`
     return this.http.get(url, {
       headers: {
         'x-api-key' : apikey
